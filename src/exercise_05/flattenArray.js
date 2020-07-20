ebcbd5ffbb2408923cecedbe28927506c9f6f0ec
leqi <michaelleqihust@gmail.com>
2020-07-20 18:05:40 +0800

    [leqi] feat: Finish exercise_05.

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -6,5 +6,25 @@ export default function flattenArray(array) {
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  // check if the input array is valid
+  if (array === undefined || array == null) {
+    throw new Error('Flatten undefined or null array');
+  }
+
+  let flattenedArray = [];
+
+  // for-loop each element in `${array}`
+  for (let element of array) {
+    // directly output if the element type is 'number'
+    if (typeof element === 'number') {
+      flattenedArray.push(element);
+    } else if (element instanceof Array) {
+      // if the element type is 'array' output each element in current element
+      for (let subElement of element) {
+        flattenedArray.push(subElement);
+      }
+    }
+  }
+
+  return flattenedArray;
 }
