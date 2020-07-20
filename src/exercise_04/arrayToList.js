fb31789febf0aa4d2c296db72b76dc59931a50e8
leqi <michaelleqihust@gmail.com>
2020-07-20 17:51:32 +0800

    [leqi] feat: Finish exercise_04.

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,38 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  // node structure of my list:
+  // next: point to next node
+  // value: the value of current node
+  class Node {
+    constructor(value, lastNode) {
+      this.value = value;
+      this.next = null;
+      if (lastNode !== null) {
+        lastNode.next = this;
+      }
+    }
+  }
+
+  // check if input parameter is valid
+  if (array === undefined || array === null) {
+    throw new Error('Creating list from undefined array');
+  }
+
+  // check if input parameter is empty
+  if (array.length == 0) {
+    throw new Error('Creating list from empty array');
+  }
+
+  let headNode = null;
+  let lastNode = null;
+  for (let num of array) {
+    let currentNode = new Node(num, lastNode);
+    lastNode = currentNode;
+    if (headNode == null) {
+      headNode = currentNode;
+    }
+  }
+
+  return headNode;
 }
