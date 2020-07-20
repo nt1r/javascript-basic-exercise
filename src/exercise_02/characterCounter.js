f402bf09d0cbad068d84d8d62aa91e641e3c32ab
leqi <michaelleqihust@gmail.com>
2020-07-20 17:14:25 +0800

    [leqi] feat: Finish exercise_02.

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -6,5 +6,25 @@ export default function countCharacter(string, prediction) {
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  let countNumber = 0;
+  // check if the input `${string}` have been defined, null or not a typeof 'String'
+  if (string === undefined || string === null || typeof string !== 'string') {
+    return countNumber;
+  }
+
+  // check if the input `${prediction}` has been defined or null
+  if (prediction === undefined || prediction === null) {
+    // if undefined, make sure every character satisfies `${prediction}`
+    prediction = () => true;
+  }
+
+  // for-loop every character in `${string}`
+  for (let char of string) {
+    // check if current character satisfies `${prediction}`
+    if (prediction(char)) {
+      countNumber++;
+    }
+  }
+
+  return countNumber;
 }
